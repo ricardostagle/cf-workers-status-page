5927e96a5f5599ba24345f5e7290ff0203cb8d62
Sanchez, Ricardo Miguel <ricardo_miguel_sanchez@baxter.com>
2025-03-28 14:01:44 -0500

    Status Page: Updated files and yarn.lock

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 // webpack.config.js
+const path = require("path");
+
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8000,
+    devMiddleware: {
+      writeToDisk: true,
+    }
+  },
   mode: "development",
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -8,8 +23,5 @@ module.exports = {
         use: 'yaml-loader'
       }
     ]
-  },
-  devServer: {
-    static: "./dist",
-  },
+  }
 }
